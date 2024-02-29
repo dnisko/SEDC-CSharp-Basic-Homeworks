6c92c8984cb1539b990f131b94a6bc4b23fa7f60
dnisko <dnisko@hotmail.com>
2024-02-29 20:37:11 +0100

    Homework2

--- Homework2/Task2/Program.cs
+++ Homework2/Task2/Program.cs
@@ -11,7 +11,7 @@
             Console.WriteLine("Enter number 2: ");
             bool inNumber2 = int.TryParse(Console.ReadLine(), out input2);
 
-            Console.WriteLine($"The first number is: {input1}\n" +
+            Console.WriteLine($"\nThe first number is: {input1}\n" +
                               $"The second number is: {input2}");
 
             int swapping = input1;
@@ -21,7 +21,7 @@
             //Intellisense  helped :)
             //(input1, input2) = (input2, input1);
 
-            Console.WriteLine("After swapping:");
+            Console.WriteLine("\nAfter swapping:");
             Console.WriteLine($"The first number is: {input1}\n" +
                               $"The second number is: {input2}");
         }
