2cd8a7cd09ae5c5d8a7d8bf6fc00358e1b8871a7
dnisko <dnisko@hotmail.com>
2024-02-29 20:31:25 +0100

    Homework2

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input1, input2, input3, input4;
+            Console.WriteLine("Enter number 1: ");
+            bool inNumber1 = int.TryParse(Console.ReadLine(), out input1);
+
+            Console.WriteLine("Enter number 2: ");
+            bool inNumber2 = int.TryParse(Console.ReadLine(), out input2);
+
+            Console.WriteLine("Enter number 3: ");
+            bool inNumber3 = int.TryParse(Console.ReadLine(), out input3);
+
+            Console.WriteLine("Enter number 4: ");
+            bool inNumber4 = int.TryParse(Console.ReadLine(), out input4);
+
+
+            int sum = input1 + input2 + input3 + input4;
+            int average = sum / 4;
+
+            Console.WriteLine($"The average of the numbers entered is: {average}");
+        }
+    }
+}
---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input1, input2;
+            Console.WriteLine("Enter number 1: ");
+            bool inNumber1 = int.TryParse(Console.ReadLine(), out input1);
+
+            Console.WriteLine("Enter number 2: ");
+            bool inNumber2 = int.TryParse(Console.ReadLine(), out input2);
+
+            Console.WriteLine($"The first number is: {input1}\n" +
+                              $"The second number is: {input2}");
+
+            int swapping = input1;
+            input1 = input2;
+            input2 = swapping;
+
+            //Intellisense  helped :)
+            //(input1, input2) = (input2, input1);
+
+            Console.WriteLine("After swapping:");
+            Console.WriteLine($"The first number is: {input1}\n" +
+                              $"The second number is: {input2}");
+        }
+    }
+}
