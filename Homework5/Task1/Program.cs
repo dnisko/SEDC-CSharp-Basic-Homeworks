d080944c5253d5d75442abfbaa5dfc55cc34da51
dnisko <dnisko@hotmail.com>
2024-03-10 22:03:47 +0100

    Homework5

---
+++ Homework5/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+
+            /*
+            Task 1
+
+            Make a method called AgeCalculator
+            The method will have one input parameter, your birthday date
+            The method should return your age
+            Show the age of a user after he inputs a date
+
+            Note: take into consideration if the birthday is today, after or before today
+            */
+
+            #endregion
+
+            while (true)
+            {
+                Console.WriteLine("Enter your birth date");
+                string inputDate = Console.ReadLine();
+
+                DateTime checkDate;
+
+                if (!DateTime.TryParse(inputDate, out checkDate))
+                {
+                    Console.WriteLine("Wrong date inserted. Try again!\n");
+                    continue;
+                }
+
+                int result = AgeCalculator(checkDate);
+
+                if (result <= 0)
+                {
+                    Console.WriteLine("You are from the future!?\n");
+                    continue;
+                }
+
+                Console.WriteLine($"Your birthdate is {inputDate} and you are {result} years old.");
+
+                break;
+            }
+
+        }
+
+        public static int AgeCalculator(DateTime a)
+        {
+            int currentDate = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int input = int.Parse(a.ToString("yyyyMMdd"));
+
+            //converting the dates in yyyyMMdd to easily subtract and divide by
+            //10000 to get the years difference, hence the age :)
+            return (currentDate - input) / 10000;
+        }
+    }
+}
---
+++ Homework5/Task2/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        #region Task2
+
+        /*
+        Task 2
+
+        Create a new console application and a new method called Substrings
+        In that method enter the following string :
+        "Hello from SEDC Codecademy 2024"
+        Ask the user to enter a number n.
+        Print the first n characters from that string and print the length of the new string.
+        Try to handle all the scenarios
+        */
+
+        #endregion
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter number to resize the string:");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int n))
+                {
+                    Console.WriteLine("Wrong input, please try again");
+                    continue;
+                }
+
+                string result = Substrings(n);
+                if (result == "-1")
+                {
+                    Console.WriteLine("The number is negative, bigger or equal to the string. Try again.\n");
+                    continue;
+                }
+
+                Console.WriteLine(result);
+
+                break;
+            }
+        }
+
+        public static string Substrings(int n)
+        {
+            string originalString = "Hello from SEDC Codecademy 2024.";
+            
+
+            if (n >= originalString.Length || n <= 0)
+            {
+                return  "-1";
+            }
+
+            string substring = originalString.Substring(n);
+            return $"The original string: |{originalString}|\n" +
+                   $"The original Length: {originalString.Length}\n\n" +
+                   $"New string: |{substring}|\n" +
+                   $"Length: {substring.Length}";
+        }
+    }
+}
